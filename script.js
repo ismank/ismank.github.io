7ab3e2d91d68cde41c22f655f7ee0114e284cfec
ismank <ismael.ouanouki@icloud.com>
2026-05-03 23:58:14 +0200

    modifying site

---
+++ script.js
@@ -0,0 +1,12 @@
+// petit effet fade-in
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+  cards.forEach(card => {
+    const rect = card.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 50) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
